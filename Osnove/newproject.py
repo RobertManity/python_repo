e39226f71f48d54f6dbf7525f41cf68593909ad5
Robert Manic <metabolizam@gmail.com>
2022-04-27 21:50:42 +0200

    Novi file

---
+++ Osnove/newproject.py
@@ -0,0 +1,23 @@
+print ("zdravo.")
+
+niz_brojevi = []
+
+print (niz_brojevi)
+
+niz_brojevi = [1,2,3]
+print (niz_brojevi)
+
+niz_brojevi = [1,2.0,"3"]
+print (niz_brojevi)
+
+niz_brojevi.append(5)
+print (niz_brojevi)
+
+print (niz_brojevi[:2])
+
+niz_brojevi[0] = 323
+print (niz_brojevi)
+
+niz_brojevi[1] = None
+
+print (niz_brojevi)
