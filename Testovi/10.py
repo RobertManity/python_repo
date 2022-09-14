e40d71cd2720e7e1cddfba40a8a9b717eccaa48c
Robert Manic <metabolizam@gmail.com>
2022-09-14 22:34:40 +0200

    Cleaning

--- Testovi/10.py
+++ Testovi/10.py
@@ -34,43 +34,4 @@ for element in a:
         print ("nasao sam ga")
         element.click()
 
-#xbtn = browser.find_element(By.XPATH, "//button[@aria-label='close']")
-#print (xbtn)
 
-
-#depart_from = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='dcep-afed358e1-6396-4a15-82be-6c4ea8fe9553-flm-flight-flightQuery.flightSegments[0].originCode']")))
-#depart_from.click()
-#depart_from.send_keys("Zurich")
-#depart_from.send_keys(Keys.ENTER)
-
-#going_to = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='dcep-afed358e1-6396-4a15-82be-6c4ea8fe9553-flm-flight-flightQuery.flightSegments[0].destinationCode']")))
-#going_to.click()
-#going_to.send_keys("New York")
-#search_results = wait.until(EC.presence_of_all_elements_located((By.XPATH)))
-
-#for results in search_results:
-
-    #if "New York (JFK)" in results.text:
-        #results.click()
-        #break
-
-#for date in all_dates:
-    #if date.get_attribute("data-date") == "23/08/2022":
-        #date.click()
-        #Break
-    #browser.find_element(By.XPATH, "//input[@value='Search Flights']").click()
-
-    #pageLength = driver.excecute_script(
-        #window.scrollTo(0, document.body.scrollHeight);var pageLength=document.body.scrollHeight;return))
-   #match = False
-    #while (match == False):
-        #lastCount = pageLength
-
-    
-
-#navigate to travel Website
-#provide going from location "Zurich"
-#provide going to location "New York"
-#click on "flight search button"
-#Select the filter "1 stop"
-#Verify that the filtered results show flights having only 1 stop
